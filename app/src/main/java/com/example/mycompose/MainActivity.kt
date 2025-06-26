234a665947f267f3dbd5e5b8c521ee94c220b301
lime13579 <128010905+lime13579@users.noreply.github.com>
2025-06-26 18:24:20 +0900

    커밋테스트

---
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.mycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycompose.ui.theme.MyComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+
+        Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(24.dp)
+        )
+    }
+
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyComposeTheme {
+//        Greeting("Android")
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun secondPreview() {
+    MyComposeTheme {
+        Greeting("name")
+    }
+}
